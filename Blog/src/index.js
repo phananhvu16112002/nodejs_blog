8074374f70332634bf04d6a23d4cd00fbbc36ca0
phananhvu16112002 <phananhvu12a5chilang@gmail.com>
2023-10-31 15:13:27 +0700

    Install Prettier,husky,and lint-staged

--- Blog/src/index.js
+++ Blog/src/index.js
@@ -35,6 +35,6 @@ app.set('views', path.join(__dirname, 'resources\\views'));
 //Route Init
       route(app);
 
-      app.listen(     port, () => {
+        app.listen(     port, () => {
           console.log(`Example app listening on port ${port}`);
 });
